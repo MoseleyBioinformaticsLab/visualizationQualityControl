507c79cb5da36f99532fdff2fc547996a1f98982
CGMossa <cgmossa@gmail.com>
2020-09-29 17:01:47 +0200

    Experimented with different ranges

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -12,20 +12,20 @@ pub enum Perspective {
 pub fn ici_kendall_tau(x: Vec<Numeric>, y: Vec<Numeric>, perspective: Perspective) -> Numeric {
     let mut sum_concordant: u64 = 0;
     let mut sum_discordant: u64 = 0;
-    let mut sum_x_ties: u64 = 0;
-    let mut sum_y_ties: u64 = 0;
+    let mut sum_x_ties: u64;
+    let mut sum_y_ties: u64;
     let mut sum_tied_x: u64 = 0;
     let mut sum_tied_y: u64 = 0;
     let mut sum_tied_x_na: u64 = 0;
     let mut sum_tied_y_na: u64 = 0;
     let mut sum_all_na: u64 = 0;
-    let is_concordant: bool;
-    let is_discordant: bool;
-    let is_tied_x: bool;
-    let is_tied_y: bool;
-    let is_tied_x_na: bool;
-    let is_tied_y_na: bool;
-    let is_all_na: bool;
+    let _is_concordant: bool;
+    let _is_discordant: bool;
+    let _is_tied_x: bool;
+    let _is_tied_y: bool;
+    let _is_tied_x_na: bool;
+    let _is_tied_y_na: bool;
+    let _is_all_na: bool;
     let k_numerator: f64;
     let k_denominator: f64;
     let k_tau: f64;
@@ -33,12 +33,10 @@ pub fn ici_kendall_tau(x: Vec<Numeric>, y: Vec<Numeric>, perspective: Perspectiv
     let mut x: Vec<f64> = x;
     let mut y: Vec<f64> = y;
 
-    let mut matching_na = (&x)
+    let matching_na = (&x)
         .iter()
         .zip((&y).iter())
-        .map(|(x, y)| x.is_nan() & y.is_nan())
-        // .collect_vec()
-        .into_iter();
+        .map(|(x, y)| x.is_nan() & y.is_nan());
 
     if let Perspective::Local = perspective {
         // remove the values, that are missing across both vectors.
@@ -62,7 +60,19 @@ pub fn ici_kendall_tau(x: Vec<Numeric>, y: Vec<Numeric>, perspective: Perspectiv
     match perspective {
         Perspective::Global => todo!(),
         Perspective::Local => {
-            for ((xi, yi), (xj, yj)) in x.iter().zip(y.iter()).tuple_combinations() {
+            // for ((xi, yi), (xj, yj)) in x.iter().zip(y.iter()).tuple_combinations() {
+            // for ((xi, yi), (xj, yj)) in x
+            //     .iter()
+            //     .cartesian_product(y.into_iter())
+            //     .tuple_combinations()
+            for (i, j) in (0..n_entry - 1)
+                .map(|x| ((x + 1)..n_entry).map(move |xx| (x, xx)))
+                .flatten()
+            {
+                let xi = x[i];
+                let xj = x[j];
+                let yi = y[i];
+                let yj = y[j];
                 sum_concordant += ((!(xi.is_nan()) && !(xj.is_nan()) && (xi > xj) && !(yi.is_nan()) && !(yj.is_nan()) && (yi > yj)) ||    // #3 1
                     (!(xi.is_nan()) && !(xj.is_nan()) && (xi < xj) && !(yi.is_nan()) && !(yj.is_nan()) && (yi < yj)) ||  // ## 2
                     (!(xi.is_nan()) && !(xj.is_nan()) && (xi > xj) && !(yi.is_nan()) && (yj.is_nan())) ||                             // ## 3
@@ -107,16 +117,16 @@ pub fn ici_kendall_tau(x: Vec<Numeric>, y: Vec<Numeric>, perspective: Perspectiv
                 sum_tied_x += (!(xi.is_nan())
                     && !(xj.is_nan())
                     // && (xi == xj)
-                    && float_eq::float_eq!(xi, xj, ulps <= 6)
+                    && float_eq::float_eq!(xi, xj, ulps <= 4)
                     // && (!(yi.is_nan()) && !(yj.is_nan()) && (yi != yj)))
-                    && (!(yi.is_nan()) && !(yj.is_nan()) && float_eq::float_ne!(yi, yj, ulps <= 6)))
+                    && (!(yi.is_nan()) && !(yj.is_nan()) && float_eq::float_ne!(yi, yj, ulps <= 4)))
                     as u64;
                 sum_tied_y += (!(xi.is_nan())
                     && !(xj.is_nan())
                     // && (xi != xj)
-                    && float_eq::float_eq!(xi, xj, ulps <= 6)
+                    && float_eq::float_ne!(xi, xj, ulps <= 4)
                     // && (!(yi.is_nan()) && !(yj.is_nan()) && (yi == yj)))
-                    && (!(yi.is_nan()) && !(yj.is_nan()) && float_eq::float_ne!(yi, yj, ulps <= 6)))
+                    && (!(yi.is_nan()) && !(yj.is_nan()) && float_eq::float_eq!(yi, yj, ulps <= 4)))
                     as u64;
                 sum_tied_x_na +=
                     ((xi.is_nan()) && (xj.is_nan()) && (!(yi.is_nan()) | !(yj.is_nan()))) as u64;
@@ -155,11 +165,28 @@ mod tests {
 
     #[test]
     fn test_speed_usecase() {
-        let mut rng = StdRng::seed_from_u64(20200929);
+        // let mut rng = StdRng::seed_from_u64(20200929);
+        let rng = thread_rng();
         let n = 1000;
-        let x = StandardNormal.sample_iter(&mut rng).take(n).collect();
-        let y = StandardNormal.sample_iter(rng).take(n).collect();
+        let x: Vec<f64> = StandardNormal.sample_iter(rng).take(n).collect();
+        let y: Vec<f64> = StandardNormal.sample_iter(rng).take(n).collect();
+
+        println!("{:?}\n{:?}", &x[..10], &y[..10]);
+
         println!("{}", ici_kendall_tau(x, y, Perspective::Local));
         // The benchmark to beat (on this machine) is 36 ms (or preferably 11 ms)
+    }
+    #[test]
+    fn test_standard_normal_many_times() {
+        // let mut rng = StdRng::seed_from_u64(20200929);
+        let rng = thread_rng();
+        let reps = 10;
+        let n = 1000;
+        for _rep in 0..reps {
+            let x: Vec<f64> = StandardNormal.sample_iter(rng).take(n).collect();
+            let y: Vec<f64> = StandardNormal.sample_iter(rng).take(n).collect();
+
+            println!("{}", ici_kendall_tau(x, y, Perspective::Local));
+        }
     }
 }
