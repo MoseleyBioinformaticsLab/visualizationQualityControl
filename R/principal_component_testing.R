2a1798052e03170d74b68c1e6f01f0184a7940ce
rmflight <r.flight79@gmail.com>
2020-10-30 15:50:36 -0400

    add direction. Loadings shouldn't be tested by direction, so that's why default is false. But there may be cases where we want it, or to recreate previous results ....

--- R/principal_component_testing.R
+++ R/principal_component_testing.R
@@ -37,10 +37,11 @@ visqc_test_pca_scores = function(pca_scores, sample_info, variables){
 #' @param loadings matrix of loadings from pca decomposition
 #' @param test_columns names of the columns of the loadings to test
 #' @param progress should progress be reported
+#' @param direction should direction of loading be tested?
 #' 
 #' @export
 #' @return named list
-visqc_test_pca_loadings = function(loadings, test_columns, progress = FALSE){
+visqc_test_pca_loadings = function(loadings, test_columns, progress = FALSE, direction = FALSE){
   if (progress) {
     n_loading = nrow(loadings) * length(test_columns)
     progress_bar = knitrProgressBar::progress_estimated(n_loading)
@@ -51,7 +52,7 @@ visqc_test_pca_loadings = function(loadings, test_columns, progress = FALSE){
     all_cols = colnames(loadings)
     use_loadings = loadings[, in_loading]
     other_loadings = loadings[, !(all_cols %in% in_loading)]
-    loading_results = test_loadings(use_loadings, other_loadings, progress_bar = progress_bar)
+    loading_results = test_loadings(use_loadings, other_loadings, progress_bar = progress_bar, direction = direction)
     loading_df = data.frame(p.value = loading_results, 
                             loading = use_loadings,
                             loading_index = in_loading,
@@ -62,17 +63,24 @@ visqc_test_pca_loadings = function(loadings, test_columns, progress = FALSE){
   loading_pvalues
 }
 
-test_loadings = function(use_loadings, other_loadings, progress_bar = NULL){
+test_loadings = function(use_loadings, other_loadings, progress_bar = NULL, direction = FALSE){
   use_index = seq(1, length(use_loadings))
   purrr::map_dbl(use_index, function(in_row){
     test_value = use_loadings[in_row]
     all_other = (as.vector(other_loadings[-in_row, ]))
     
-    if (test_value < 0) {
-      is_more = sum(all_other < test_value) / sum(all_other < 0)
+    if (direction) {
+      if (test_value < 0) {
+        is_more = sum(all_other < test_value) / sum(all_other < 0)
+      } else {
+        is_more = sum(all_other > test_value) / sum(all_other >= 0)
+      }
     } else {
-      is_more = sum(all_other > test_value) / sum(all_other >= 0)
+      all_other = abs(all_other)
+      test_value = abs(test_value)
+      is_more = sum(all_other > test_value) / length(all_other)
     }
+    
     knitrProgressBar::update_progress(progress_bar)
     is_more
   })
